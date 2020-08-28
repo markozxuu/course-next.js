3b29aa149b31747b4a4e2b722f63cd595276a710
markoz <markdrew53@gmail.com>
2020-08-28 15:30:54 -0500

    Added header component

---
+++ components/Header.js
@@ -0,0 +1,31 @@
+// Packages
+import Link from 'next/link'
+import { useSession, signIn, signOut } from 'next-auth/client'
+
+export default function Header () {
+  const [session] = useSession()
+  const handleLogin = (event) => {
+    event.preventDefault();
+    signIn('github')
+
+  }
+  const handleLogout = (event) => {
+    event.preventDefault();
+    signOut()
+  }
+  return (
+    <div className='header'>
+      <Link href='/'>
+        <a className='title'>My Blog</a>
+      </Link>
+      {session ? (
+         <>
+         <img src={session.user.image} className="user-info"/>
+         <a href="#" onClick={handleLogout} className="logout">Logout</a>
+       </>
+      ) : (
+        <a href="#" onClick={handleLogin} className="logout">Login</a>
+      )}
+    </div>
+  )
+}
